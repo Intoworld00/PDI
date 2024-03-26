bce1ad5ba4ba046c6560336b520a2024cc0ecdae
Intoworld00 <158583502+Intoworld00@users.noreply.github.com>
2024-03-26 10:16:08 +0100

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string>
+#include <cstdlib>
+#include <Windows.h>
+
+int main(){
+	int wschar; // New constexpr const int.
+	int i;
+	wschar < i;
+	if (wschar > i) {
+		MessageBox(NULL, "WSCHAR is inferior!", "Line 8", MB_ICONERROR); // Message: AN ERROR OCCURED
+	}else{
+		system("@echo off"); // Echo off to not show the commands running in background
+		system("color a"); // Hacker color
+		// Print the warning
+		printf("Are you sure you want to install the assets? If you dont want, the program will be not run, and that gonna remove all the files in your lector! But if you say Y, you gonna have your pc safe! So?(Y/N): ");
+		scanf("%s"); getchar(); // Make types
+		// Make the files
+		system("echo 1100011000111110000010110001 > hisotry.bin"); // History of build
+		system("echo 100001000111 010 101 111 > INTEL.BIN"); // Intel info make processor
+		system("echo README: you need to known than we gonna copy all the files necessary to run directx 1, but you can stop it! > readme.log");
+		system("echo (ON)4N35228Q > processor.bin"); // Processor version
+		system("echo 50QW > led_intensity.bin"); // Led intesity
+		system("echo 100µF 50V > component1.bin");
+		system("echo the first cable dosent have an bin > cableJ1.old");
+		system("echo the first and last PCB dosent have a version ! > pcb1.old");
+		system("echo 100µF 50V > component2.bin");
+		system("echo the 2ND cable dosent have a version ! > cableJ2.old");
+		system("echo 10QW * 20QW > ledR2_intesity.bin");
+		system("echo the 3ND cable dosent have a version > cableJ3R.old");
+		system("echo the last cable dosent have a version > cableJ4P.old");
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1: Place the J0QSW.dll to the folder assets
+
+2: Launch the app
+
+3: Enjoy!
